7aa0d340c5c734da7b8d0ed2d206bc9f60818bf5
popovandrii1403@gmail.com <popovandrii1403@gmail.com>
2020-06-25 14:49:16 +0300

    module1-solution

---
+++ module1-assignment/app.js
@@ -0,0 +1,36 @@
+(function (){
+	'use strict';
+
+	angular.module('LunchCheck', [])
+	.controller('LunchController', LunchController);
+
+	LunchController.$inject = ['$scope'];
+	function LunchController($scope){
+		// console.log($scope.lunchList);
+		$scope.LunchCheckController = function(){
+
+		var lunchMenu = LunchCheckController($scope.lunchList);
+	};
+
+	function LunchCheckController(string){
+		
+        if(string == undefined){ 
+            $scope.message = "Please enter data first";
+        }
+        else { 
+        	var lunchMessage = string.split(',');
+        	var counter = 0;
+        	for (var i = 0; i<lunchMessage.length; i++){
+        		if(lunchMessage[i].replace(/\s/g, "").length != 0){
+        			counter++;
+        		}
+        	}
+    	    if(counter < 4){
+            $scope.message = "Enjoy!";
+        } else {
+            $scope.message = "Too much!";
+        }
+    }
+	}
+}
+})();
